ac43f3f98a35102091b592f66a6c94a4a7578c20
sutharsan <theepansutharsan1117@gmail.com>
2026-06-09 03:51:19 +0530

    connect database

--- run.py
+++ run.py
@@ -7,5 +7,14 @@ def home ():
     return '<h1>This is Home Page</h1>'
 
 if __name__ == "__main__":
+    try:
+        with app.app_context():
+            from app.extensions import db
+            db.create_all() 
+            print("SUCCESS: Database Connected Successfully")
+           
 
+    except Exception as e:
+        print("ERROR: Database Connection Failed")
+        print(e)
     app.run(debug=True,port=8080)
