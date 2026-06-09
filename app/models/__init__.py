6e9341dda3999614b43a26d92d10a8113b90c77b
sutharsan <theepansutharsan1117@gmail.com>
2026-06-09 12:57:47 +0530

    add some changes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.student_model import Student
 from app.models.lecturer_model import Lecturer
+from app.models.course_model import Course  
 
-__all__ = ["Student", "Lecturer"]
+__all__ = ["Student", "Lecturer", "Course"]
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from app.routes.student_routes import student_bp
 from app.routes.lecturer_routes import lecturer_bp
+from app.routes.course_routes import course_bp
 def register_blueprints(app):
     app.register_blueprint(student_bp)
     app.register_blueprint(lecturer_bp)
+    app.register_blueprint(course_bp)
